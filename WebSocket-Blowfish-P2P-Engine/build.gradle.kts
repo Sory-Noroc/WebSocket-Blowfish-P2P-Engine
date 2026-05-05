a54b86f8af4f62a816636aa5b6f6998d28a4f325
Sory-Noroc <sorinnoroc1@gmail.com>
2026-05-05 13:15:00 +0300

    Web UI error fix

--- WebSocket-Blowfish-P2P-Engine/build.gradle.kts
+++ WebSocket-Blowfish-P2P-Engine/build.gradle.kts
@@ -22,20 +22,23 @@ kotlin {
 }
 
 dependencies {
+    // BOM-ul gestionează versiunile pentru toate pachetele Ktor
     implementation(platform("io.ktor:ktor-bom:3.4.0"))
+
     implementation("io.ktor:ktor-server-core")
-    implementation("io.ktor:ktor-server-websockets")
     implementation("io.ktor:ktor-server-netty")
-    implementation("io.ktor:ktor-server-static-content")
-    implementation("ch.qos.logback:logback-classic:$logback_version")
+    implementation("io.ktor:ktor-server-websockets")
     implementation("io.ktor:ktor-server-config-yaml")
+
+    implementation("io.ktor:ktor-server-content-negotiation")
     implementation("io.ktor:ktor-serialization-kotlinx-json")
+
+    implementation("ch.qos.logback:logback-classic:$logback_version")
 
     testImplementation("io.ktor:ktor-server-test-host")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testImplementation("org.junit.jupiter:junit-jupiter:5.10.2")
 }
-
 tasks.test {
     useJUnitPlatform()
 }
--- WebSocket-Blowfish-P2P-Engine/src/main/kotlin/Application.kt
+++ WebSocket-Blowfish-P2P-Engine/src/main/kotlin/Application.kt
@@ -7,6 +7,10 @@ import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
 
 fun main() {
+
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
+        .start(wait = true)
+
     val subnet = "10.91.198.0/24"
     val discoveredIps = NetworkUtils.discoverPeers(subnet)
 
@@ -16,9 +20,6 @@ fun main() {
     } else {
         println("Niciun partener găsit. Aștept conexiuni...")
     }
-
-    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
-        .start(wait = true)
 }
 
 fun Application.module() {
